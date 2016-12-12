07e34a0024e84e5f59a4fc6729e5347f80592d72
Air <hayrider@yandex.ru>
2016-12-13 02:32:54 +0300

    5

---
+++ SE01/src/main/java/Task5.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Air on 13/12/2016.
+ */
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        printMatrix(generateMatrix(11));
+
+    }
+
+    private static int[][] generateMatrix(int size) {
+        int[][] arr = new int[size][size];
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if (x == y) {
+                    arr[y][x] = 1;
+                    arr[y][size - x - 1] = 1;
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static void printMatrix(int[][] arr) {
+        for (int[] row: arr) {
+            for (int cell: row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
