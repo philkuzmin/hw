a4dcad96f100e87a0685e3d6d480a32243f5d2b7
Phil <hayrider@yandex.ru>
2016-12-13 00:33:55 +0300

    2

--- SE01/src/main/java/Task2.java
+++ SE01/src/main/java/Task2.java
@@ -5,15 +5,16 @@ public class Task2 {
 
     public static void main(String[] args) {
 
-        double eps = 0.26;
+        double eps = 0.009;
         double result;
-        int i = 1;
+        int i = 0;
 
         do {
             result = getElement(i);
-            System.out.printf("a%d = %.6f\n", i++, result);
-        } while (i<10);
+            System.out.printf("a%d = %.10f\n", ++i, result);
+        } while (result > eps);
 
+        System.out.println("The smallest number is " + i);
     }
 
     static double getElement(int n) {
